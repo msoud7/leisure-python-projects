32ec18fe2afcbabcc20e4557c619ee4f110b752d
msoud7 <136334443+msoud7@users.noreply.github.com>
2024-05-09 01:19:01 +0200

    Added clickablility

--- calculator_project/calculator.py
+++ calculator_project/calculator.py
@@ -15,9 +15,9 @@ def divide(x, y):
     return x / y
 
 # Set up screen
-screen = turtle.Screen()
-screen.setup(width=500, height=500)
-screen.title("Catculator")
+screen = turtle.Screen() #initialize the base
+screen.setup(width=500, height=500) #set the size of the base
+screen.title("Catculator") #name the application
 
 # Take input for x and y
 x = turtle.textinput("Enter the first number", "Enter a number")
@@ -28,33 +28,37 @@ x = float(x)
 y = float(y)
 
 # Register shapes
-screen.register_shape("addition.gif")
+screen.register_shape("addition.gif") #register size
 screen.register_shape("subtraction.gif")
 screen.register_shape("divide.gif")
 screen.register_shape("multiplication.gif")
 
 # Create image turtles
-addition_turtle = turtle.Turtle()
-subtraction_turtle = turtle.Turtle()
-divide_turtle = turtle.Turtle()
-multiplication_turtle = turtle.Turtle()
+addition_turtle = turtle.Turtle() #create base shape 1
+subtraction_turtle = turtle.Turtle() #create base shape 2
+divide_turtle = turtle.Turtle() #create base shape 3
+multiplication_turtle = turtle.Turtle() #create base shape 4
 
 # Set positions for the image turtles
-addition_turtle.shape("addition.gif")
-addition_turtle.penup()
-addition_turtle.goto(-140, 80)
+addition_turtle.shape("addition.gif") #add the gif to the base shape
+addition_turtle.penup() #stop drawing
+addition_turtle.goto(-140, 80) #displace
+addition_turtle.onclick(add)
 
 subtraction_turtle.shape("subtraction.gif")
 subtraction_turtle.penup()
 subtraction_turtle.goto(140, 80)
+subtraction_turtle.onclick(subtract)
 
 divide_turtle.shape("divide.gif")
 divide_turtle.penup()
 divide_turtle.goto(-140, -140)
+divide_turtle.onclick(divide)
 
 multiplication_turtle.shape("multiplication.gif")
 multiplication_turtle.penup()
 multiplication_turtle.goto(140, -140)
+multiplication_turtle.onclick(multiply)
 
 # Keep the window open
 turtle.done()
